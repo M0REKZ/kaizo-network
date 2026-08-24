34fc8ff21d73b2ef303cfd8492c373ebf91b86e8
Dennis Felsing <dennis@felsing.org>
2026-08-24 15:59:23 +0200

    client: Handle input earlier
    People always complained that lower refresh rate increased input latency
    and I assumed it's inherent. This improves the situation by sending the
    inputs one loop iteration earlier. At cl_refresh_rate 60 the input is
    sent 16ms earlier.

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -2928,6 +2928,12 @@ void CClient::Update()
 {
 	PumpNetwork();
 
+	// update editor/gameclient, before input snapping
+	if(m_EditorActive)
+		m_pEditor->OnUpdate();
+	else
+		GameClient()->OnUpdate();
+
 	if(State() == IClient::STATE_DEMOPLAYBACK)
 	{
 		if(m_DemoPlayer.IsPlaying())
@@ -3217,12 +3223,6 @@ void CClient::Update()
 
 	// update the server browser
 	m_ServerBrowser.Update();
-
-	// update editor/gameclient
-	if(m_EditorActive)
-		m_pEditor->OnUpdate();
-	else
-		GameClient()->OnUpdate();
 
 	Discord()->Update();
 	Steam()->Update();
